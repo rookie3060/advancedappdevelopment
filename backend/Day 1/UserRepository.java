045beaa0560cfb04acae953c9485e6f9f16072f0
rookie3060 <98405422+rookie3060@users.noreply.github.com>
2024-03-05 12:36:49 +0530

    Delete backend/Day 1 directory

--- backend/Day 1/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.agricultureloan.agricultureloan.Repository;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.agricultureloan.agricultureloan.model.User;
-
-public interface UserRepository extends JpaRepository<User,Long> {
-    
-}
