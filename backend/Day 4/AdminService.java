b50381cb51d01814531ac7117b76217639e1a830
rookie3060 <98405422+rookie3060@users.noreply.github.com>
2024-03-05 12:37:20 +0530

    Delete backend/Day 4 directory

--- backend/Day 4/AdminService.java
+++
@@ -1,5 +0,0 @@
-package com.event.kaushik.service;
-import com.event.kaushik.DTO.AdminDto;
-public interface AdminService {
-    void createEvent(AdminDto admindto);
-}
--- backend/Day 4/EventService.java
+++
@@ -1,5 +0,0 @@
-package com.event.kaushik.service;
-import com.event.kaushik.DTO.EventDto;
-public interface EventService {
-    void createEvent(EventDto eventdto);
-}
